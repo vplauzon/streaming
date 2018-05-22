5c5c4c2e0841311b3c848e1fadac9b321100424e
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-05-22 13:32:43 -0400

    2 Event Hubs namespaces

--- SummaryStreaming/Deployment/Deploy-AzureResourceGroup.ps1
+++ SummaryStreaming/Deployment/Deploy-AzureResourceGroup.ps1
@@ -2,7 +2,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'SummaryStreaming',
+    [string] $ResourceGroupName = 'summary',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
