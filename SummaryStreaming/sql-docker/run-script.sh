6487f9f238224a269df512b038115053903e4329
vplauzon <vincent_lauzon@yahoo.com.au>
2018-05-28 16:26:44 -0400

    .

--- SummaryStreaming/sql-docker/run-script.sh
+++ SummaryStreaming/sql-docker/run-script.sh
@@ -1,5 +1,13 @@
 echo "Fetch " + $SCRIPT_URL
 
-#curl $SCRIPT_URL > script.sql
+curl $SCRIPT_URL > script.sql
 
-#sqlcmd -S $SQL_SERVER -d $SQL_DB -U $SQL_USER_NAME -P  SQL_PASSWORD -i script.sql
+echo "Script fetched:"
+
+cat script.sql
+
+echo
+
+echo "SQL CMD on the script"
+
+sqlcmd -S $SQL_SERVER -d $SQL_DB -U $SQL_USER_NAME -P  SQL_PASSWORD -i script.sql
