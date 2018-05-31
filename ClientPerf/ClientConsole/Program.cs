676223dbfffe595758c5b2baff81d9e10f360fd5
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-05-31 13:22:20 -0400

    First test

--- ClientPerf/ClientConsole/Program.cs
+++ ClientPerf/ClientConsole/Program.cs
@@ -1,14 +1,64 @@
-﻿using System;
+﻿using Microsoft.Azure.EventHubs;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ClientConsole
 {
     class Program
     {
+        const string CONNECTION_STRING = "Endpoint=sb://eh-rovdymiz3vorc.servicebus.windows.net/;SharedAccessKeyName=sendRule;SharedAccessKey=AJYFHvxEEtg/+b+o8pI6divfqf/Jshqjk1//kOakso0=;EntityPath=test-hub";
+
         static void Main(string[] args)
         {
-            const string CONNECTION_STRING = "Endpoint=sb://eh-rovdymiz3vorc.servicebus.windows.net/;SharedAccessKeyName=sendRule;SharedAccessKey=AJYFHvxEEtg/+b+o8pI6divfqf/Jshqjk1//kOakso0=;EntityPath=test-hub";
+            for (int i = 0; i != 10; ++i)
+            {
+                Console.WriteLine($"One Event:  {TimeFunctionAsync(SendOneEventAsync).Result}");
+            }
+        }
 
-            Console.WriteLine("Hello World!");
+        private async static Task SendOneEventAsync()
+        {
+            var client = EventHubClient.CreateFromConnectionString(CONNECTION_STRING);
+
+            try
+            {
+                await client.SendAsync(new EventData(GetDummyEventBinary()));
+            }
+            finally
+            {
+                await client.CloseAsync();
+            }
+        }
+
+        private static byte[] GetDummyEventBinary()
+        {
+            var dummyEvent = new
+            {
+                Name = "John Smith",
+                CreatedAt = DateTime.UtcNow.ToString("o")
+            };
+            var serializer = new JsonSerializer();
+            var stream = new MemoryStream();
+            var streamWriter = new StreamWriter(stream);
+            var jsonWriter = new JsonTextWriter(streamWriter);
+
+            serializer.Serialize(jsonWriter, dummyEvent);
+            streamWriter.Flush();
+
+            return stream.GetBuffer();
+        }
+
+        private async static Task<TimeSpan> TimeFunctionAsync(Func<Task> function)
+        {
+            var watch = new Stopwatch();
+
+            watch.Start();
+            await function();
+
+            return watch.Elapsed;
         }
     }
 }
--- ClientPerf/README.md
+++ ClientPerf/README.md
@@ -2,6 +2,8 @@
 
 Here we analyze different approaches to send events to Azure Event Hubs.  We look at the client performance.
 
+We use a Docker Container to run tests.  The container is defined in this folder (i.e. it isn't a Visual Studio project).
+
 <a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fvplauzon%2Fstreaming%2Fmaster%2FClientPerf%2FDeployment%2Fazuredeploy.json" target="_blank">
     <img src="http://azuredeploy.net/deploybutton.png"/>
 </a>
