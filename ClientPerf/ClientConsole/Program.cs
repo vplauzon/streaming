e611089168ec52c9dac93b63f7d41585c3739c2c
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-05-31 10:28:22 -0400

    Add event hubs deployment

--- ClientPerf/ClientConsole/Program.cs
+++ ClientPerf/ClientConsole/Program.cs
@@ -6,6 +6,8 @@ namespace ClientConsole
     {
         static void Main(string[] args)
         {
+            const string CONNECTION_STRING = "Endpoint=sb://eh-rovdymiz3vorc.servicebus.windows.net/;SharedAccessKeyName=sendRule;SharedAccessKey=AJYFHvxEEtg/+b+o8pI6divfqf/Jshqjk1//kOakso0=;EntityPath=test-hub";
+
             Console.WriteLine("Hello World!");
         }
     }
