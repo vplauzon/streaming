2fec5a652b55546535dd25c3594eca73d63c31b2
vplauzon <vincent_lauzon@yahoo.com.au>
2018-05-31 14:04:53 -0400

    .

--- ClientPerf/test-docker/build-container.sh
+++ ClientPerf/test-docker/build-container.sh
@@ -1,4 +1,8 @@
 #	Build Console App in release mode
 dotnet build ../../ClientPerf/ -c Release
 
-cp 
+#	Copy dll
+cp ../ClientConsole/bin/Release/netcoreapp2.0/ClientConsole.dll .
+
+#	Build docker container
+sudo docker build -t client-perf-event-hub .
