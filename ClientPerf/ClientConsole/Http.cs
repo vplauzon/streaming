9be1a6c862741535aa299cec9ce5c09d4661ab24
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-06-01 10:52:38 -0400

    Implement Http-interface

--- ClientPerf/ClientConsole/Http.cs
+++ ClientPerf/ClientConsole/Http.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.Azure.EventHubs;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace ClientConsole
 {
@@ -30,11 +33,9 @@ namespace ClientConsole
         {
             var builder = new EventHubsConnectionStringBuilder(ConnectionString);
             var nameSpace = builder.Endpoint.Authority.Split('.')[0];
-            var client = new HttpClient
-            {
-                BaseAddress = new Uri($"https://{nameSpace}.servicebus.windows.net/")
-            };
-            var url = $"{builder.EntityPath}/messages";
+            var client = new HttpClient();
+            var resourceUrl = $"https://{nameSpace}.servicebus.windows.net/{builder.EntityPath}";
+            var url = $"{resourceUrl}/messages";
             var content = new StringContent(
                 GetDummyEventString(),
                 Encoding.UTF8,
@@ -42,10 +43,23 @@ namespace ClientConsole
 
             client.DefaultRequestHeaders.TryAddWithoutValidation(
                 "Authorization",
-                $"SharedAccessSignature sr={builder.SasKeyName}");
+                CreateToken(resourceUrl, builder.SasKeyName, builder.SasKey));
 
             var response = await client.PostAsync(url, content);
             var responseText = await response.Content.ReadAsStringAsync();
+        }
+
+        private static string CreateToken(string resourceUri, string keyName, string key)
+        {
+            TimeSpan sinceEpoch = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            var week = 60 * 60 * 24 * 7;
+            var expiry = Convert.ToString((int)sinceEpoch.TotalSeconds + week);
+            string stringToSign = HttpUtility.UrlEncode(resourceUri) + "\n" + expiry;
+            HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(key));
+            var signature = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(stringToSign)));
+            var sasToken = String.Format(CultureInfo.InvariantCulture, "SharedAccessSignature sr={0}&sig={1}&se={2}&skn={3}", HttpUtility.UrlEncode(resourceUri), HttpUtility.UrlEncode(signature), expiry, keyName);
+
+            return sasToken;
         }
     }
 }
