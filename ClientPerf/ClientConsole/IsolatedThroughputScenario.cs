b3ec015b4e234c0568b5b9528a7ec58c52cbd191
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-06-02 08:17:28 -0400

    Confirm isolated throughput

--- ClientPerf/ClientConsole/IsolatedThroughputScenario.cs
+++ ClientPerf/ClientConsole/IsolatedThroughputScenario.cs
@@ -17,17 +17,24 @@ namespace ClientConsole
 
         public override async Task RunAsync()
         {
-            var cancellationTokenSource = new CancellationTokenSource(SAMPLING_TIME);
-            var threads = (from i in Enumerable.Range(0, THREAD_COUNT)
-                           select OneThreadAsync(cancellationTokenSource.Token)).ToArray();
+            int count = 0;
+            var elapsed = await TimeFunctionAsync(async () =>
+            {
+                var cancellationTokenSource = new CancellationTokenSource(SAMPLING_TIME);
+                var threads = (from i in Enumerable.Range(0, THREAD_COUNT)
+                               select OneThreadAsync(cancellationTokenSource.Token)).ToArray();
 
-            await Task.WhenAll(threads);
+                await Task.WhenAll(threads);
 
-            var counts = from t in threads
-                         select t.Result;
-            var count = counts.Sum();
+                var counts = from t in threads
+                             select t.Result;
+
+                count = counts.Sum();
+            });
 
             Console.WriteLine($"Total Events:  {count}");
+            Console.WriteLine($"Duration:  {elapsed}");
+            Console.WriteLine($"Throughput:  {(double)count / elapsed.TotalSeconds}");
         }
 
         private async Task<int> OneThreadAsync(CancellationToken cancellationToken)
