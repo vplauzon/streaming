cf04541b7d1cdd54b60cce87cf443811eb80b9bf
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2020-03-01 09:37:54 -0500

    Create skeleton of test app

---
+++ iot-watch-current-load/code/iot-watch-current-load-solution/MultiPerfClient/HubFeeder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MultiPerfClient
+{
+    internal class HubFeeder
+    {
+        public static Task RunAsync()
+        {
+            Console.WriteLine("Hub Feeder");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ iot-watch-current-load/code/iot-watch-current-load-solution/MultiPerfClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MultiPerfClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var mode = Environment.GetEnvironmentVariable("MODE");
+
+            Console.WriteLine("MultiPerfClient");
+
+            switch (mode?.Trim())
+            {
+                case "hub-feeder":
+                    await HubFeeder.RunAsync();
+                    
+                    return;
+
+                default:
+                    Console.WriteLine("Environment variable 'MODE' must be set to one of the following values:");
+                    Console.WriteLine("* 'hub-feeder', for feeding an IoT hub");
+                    Console.WriteLine();
+                    if (!string.IsNullOrWhiteSpace(mode))
+                    {
+                        Console.WriteLine($"Mode '{mode}' isn't supported");
+                    }
+                    
+                    return;
+            }
+        }
+    }
+}
