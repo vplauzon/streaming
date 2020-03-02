5569960de7c4b839c71210745f75f637a3c3ec69
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2020-03-01 19:55:46 -0500

    .

--- iot-watch-current-load/README.md
+++ iot-watch-current-load/README.md
@@ -3,3 +3,5 @@
 This is a load test to simulate a bunch of devices pushing telemetry to Azure IoT hub.  The telemetry is then ingested in some data service and random poke are done at the "current" (i.e. most up-to-date) telemetry.
 
 We are comparing two data services:  Azure Cosmos DB & Azure Data Explorer.
+
+Register devices:  https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-devguide-security#c-support
--- iot-watch-current-load/code/iot-watch-current-load-solution/MultiPerfClient/HubFeeder.cs
+++ iot-watch-current-load/code/iot-watch-current-load-solution/MultiPerfClient/HubFeeder.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Azure.Devices;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MultiPerfClient
@@ -9,11 +11,52 @@ namespace MultiPerfClient
     /// </summary>
     internal class HubFeeder
     {
-        public static Task RunAsync()
+        public static async Task RunAsync()
         {
             Console.WriteLine("Hub Feeder");
 
-            return Task.CompletedTask;
+            var connectionString = Environment.GetEnvironmentVariable("IOT_CONN_STRING");
+            var deviceCountText = Environment.GetEnvironmentVariable("DEVICE_COUNT");
+            int deviceCount;
+
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentNullException("Env Var 'IOT_CONN_STRING' not set", "IOT_CONN_STRING");
+            }
+            else if (string.IsNullOrWhiteSpace(deviceCountText))
+            {
+                throw new ArgumentNullException("Env Var 'DEVICE_COUNT' not set", "DEVICE_COUNT");
+            }
+            else if (!int.TryParse(deviceCountText, out deviceCount))
+            {
+                throw new ArgumentException("Env Var 'DEVICE_COUNT' isn't an integer", "DEVICE_COUNT");
+            }
+            else
+            {
+                await RegisterDevicesAsync(connectionString, deviceCount);
+            }
+        }
+
+        private async static Task RegisterDevicesAsync(string connectionString, int deviceCount)
+        {
+            var registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            var tasks = (from i in Enumerable.Range(0, deviceCount)
+                         select RegisterDeviceAsync(registryManager, i)).ToArray();
+
+            await Task.WhenAll(tasks);
+        }
+
+        private async static Task RegisterDeviceAsync(RegistryManager registryManager, int index)
+        {
+            var device = new Device("mydev" + index)
+            {
+                Authentication = new AuthenticationMechanism
+                {
+                    SymmetricKey = new SymmetricKey()
+                }
+            };
+
+            await registryManager.AddDeviceAsync(device);
         }
     }
 }
