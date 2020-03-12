a663845df2073d28a22a120a0e0d86a7033272ac
vplauzon <vplauzon@vplauzon.com>
2020-03-12 19:06:01 -0400

    Add Cosmos DB in deployment

--- iot-watch-current-load/deploy.sh
+++ iot-watch-current-load/deploy.sh
@@ -6,14 +6,18 @@
 ##  Takes 1 parameter:
 ##
 ##  1- Name of resource group
+##  2- Deployment Type:  iotOnly, cosmos, adx or all
 
 rg=$1
+type=$2
 
 echo "Resource group:  $rg"
+echo "Deployment Type:  $type"
 
 echo
 echo "Deploying ARM template"
 
 az group deployment create -n "deploy-$(uuidgen)" -g $rg \
-    --template-file deploy.json
+    --template-file deploy.json \
+    --parameters deploymentType=$type
 
